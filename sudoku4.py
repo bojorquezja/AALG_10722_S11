21c8c8bf0ce3f8c5bf5a2498c189d569c96d13e9
bojorquezja <bojorquez.ja@gmail.com>
2025-11-01 00:28:34 -0500

    sudoku 4 resuelto

--- sudoku4.py
+++ sudoku4.py
@@ -19,8 +19,8 @@ def valida(mat,f, c, val):
             return False
     nc = c//2
     nf = f//2
-    minc, maxc = 0,2 if nc == 0 else 2,4
-    minf, maxf = 0,2 if nf == 0 else 2,4
+    minc, maxc = (0,2) if nc == 0 else (2,4)
+    minf, maxf = (0,2) if nf == 0 else (2,4)
     
     for fil in range(minf, maxf):
         for col in range(minc, maxc):
@@ -28,31 +28,34 @@ def valida(mat,f, c, val):
                 return False
     return True
 
-def sudoku4(lab ):
-    f, c = sigPosDisp(lab)
+def sudoku4(mat ):
+    
+    f, c = sigPosDisp(mat)
+    #print(f"sig ({f},{c})")
     if f == -1 and c == -1:
         return True
     else:
         for val in range(1,4+1):
-            if valida(lab, f, c, val):
-                lab[f][c] = val
-                impmat(lab)
-                #pendiente
-                return sudoku4(lab )
+            #print(f"P{f},{c} = {val}")
+            if valida(mat, f, c, val):
+                mat[f][c] = val
+                impmat(mat)
+                if sudoku4(mat ):
+                    return True
                 else:
-                    res[f][c] = 0
+                    mat[f][c] = 0
                     return False
-            else:
-                return False
-lab = [
+        return False
+
+mat = [
     [0,0,2,0],
     [4,2,0,0],
     [0,0,0,0],
     [2,0,0,0]
 ]
-
-if labbas(lab, 0, 0):
-    impmat(res)
-    print("Salimos")
+print(f"X{0},{0} = {mat[0][0]}")
+if sudoku4(mat):
+    impmat(mat)
+    print("Solucion")
 else:
     print("Ayuda, no hay salida")
